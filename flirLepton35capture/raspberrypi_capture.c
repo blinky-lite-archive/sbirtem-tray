3d9fd08cb7ed0836d509b8bcf4dd8d7502a65cf5
David McGinnis <david.mcginnis@blinky-lite.io>
2020-11-08 10:25:47 +0100

    working lepton3p5

---
+++ flirLepton35capture/raspberrypi_capture.c
@@ -0,0 +1,224 @@
+/*
+Copyright (c) 2014, Pure Engineering LLC
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+#include <limits.h>
+
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0') 
+
+static void pabort(const char *s)
+{
+	perror(s);
+	abort();
+}
+
+static const char *device = "/dev/spidev0.1";
+static uint8_t mode;
+static uint8_t bits = 8;
+static uint32_t speed = 20000000;
+static uint16_t delay = 0;
+
+#define VOSPI_FRAME_SIZE (164)
+uint8_t lepton_frame_packet[VOSPI_FRAME_SIZE];
+static unsigned int lepton_image[4][60][80];
+
+static void save_pgm_file(char* imageName)
+{
+	int i;
+	int j;
+    int is;
+    int ieven;
+	unsigned int maxval = 0;
+	unsigned int minval = UINT_MAX;
+
+	FILE *f = fopen(imageName, "w");
+	if (f == NULL)
+	{
+		printf("Error opening file!\n");
+		exit(1);
+	}
+
+	printf("Calculating min/max values for proper scaling...\n");
+    
+	for(is=0;is<4;is++)
+	{
+	    for(i=0;i<60;i++)
+	    {
+		    for(j=0;j<80;j++)
+		    {
+			    if (lepton_image[is][i][j] > maxval) {
+				    maxval = lepton_image[is][i][j];
+			    }
+			    if (lepton_image[is][i][j] < minval) {
+				    minval = lepton_image[is][i][j];
+			    }
+		    }
+	    }
+    }
+	printf("maxval = %u\n",maxval);
+	printf("minval = %u\n",minval);
+	
+	fprintf(f,"P2\n160 120\n%u\n",maxval-minval);
+	for(is=0;is<4;is++)
+	{
+        ieven = 0;
+	    for(i=0;i<60;i++)
+	    {
+		    for(j=0;j<80;j++)
+		    {
+			    fprintf(f,"%d ", lepton_image[is][i][j] - minval);
+		    }
+		    if (ieven > 0) fprintf(f,"\n");
+            ++ieven;
+            if (ieven > 1) ieven = 0;
+	    }
+    }
+	fprintf(f,"\n\n");
+
+	fclose(f);
+}
+
+int transfer(int fd)
+{
+	int ret;
+	int icol;
+	int irow = 0;
+    uint8_t iseg;
+    uint8_t segRead = 0;
+	uint8_t tx[VOSPI_FRAME_SIZE] = {0, };
+	struct spi_ioc_transfer tr = {
+		.tx_buf = (unsigned long)tx,
+		.rx_buf = (unsigned long)lepton_frame_packet,
+		.len = VOSPI_FRAME_SIZE,
+		.delay_usecs = delay,
+		.speed_hz = speed,
+		.bits_per_word = bits,
+	};
+
+    iseg = 0;
+    while (iseg < 4)
+    {
+        irow = 0;
+        while (irow != 59)
+        {
+            ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+//            if (ret < 1) pabort("can't send spi message");
+
+	        if(((lepton_frame_packet[0]&0xf) != 0x0f))
+	        {
+	            irow = lepton_frame_packet[1];
+                if (irow == 20) segRead = lepton_frame_packet[0] >> 4; 
+    //                printf("boog %c%c%c%c %c%c%c%c %c%c%c%c %c%c%c%c\n", BYTE_TO_BINARY(lepton_frame_packet[0]), BYTE_TO_BINARY(lepton_frame_packet[1]));
+                if (irow < 60)
+                {
+	                for(icol = 0; icol < 80; icol++)
+	                {
+                        lepton_image[iseg][irow][icol] = (lepton_frame_packet[2 * icol + 4] << 8 | lepton_frame_packet[2 * icol + 5]);
+	                }
+                }
+	        }
+        }
+//        printf("%d\n",segRead);
+        if (segRead == (iseg + 1)) ++iseg;
+    }
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	int ret = 0;
+	int fd;
+
+	fd = open(device, O_RDWR);
+	if (fd < 0)
+	{
+		pabort("can't open device");
+	}
+
+	ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
+	if (ret == -1)
+	{
+		pabort("can't set spi mode");
+	}
+
+	ret = ioctl(fd, SPI_IOC_RD_MODE, &mode);
+	if (ret == -1)
+	{
+		pabort("can't get spi mode");
+	}
+
+	ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+	if (ret == -1)
+	{
+		pabort("can't set bits per word");
+	}
+
+	ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
+	if (ret == -1)
+	{
+		pabort("can't get bits per word");
+	}
+
+	ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+	if (ret == -1)
+	{
+		pabort("can't set max speed hz");
+	}
+
+	ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
+	if (ret == -1)
+	{
+		pabort("can't get max speed hz");
+	}
+
+	printf("spi mode: %d\n", mode);
+	printf("bits per word: %d\n", bits);
+	printf("max speed: %d Hz (%d KHz)\n", speed, speed/1000);
+    transfer(fd);
+
+	close(fd);
+
+	save_pgm_file("pic0.pgm");
+
+	return ret;
+}
